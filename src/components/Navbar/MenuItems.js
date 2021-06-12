64f2299f64001732b2b3572183f749b9006226d3
Stephan Iskander <stephan.iskander@gmail.com>
2021-06-12 11:25:25 -0400

    Adding in the menu items in the form of an array

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,32 @@
+export const MenuItems = [
+    {
+        title: 'About Me',
+        url: '#',
+        cName: 'nav-Links'
+    },
+    {
+        title: 'Projects',
+        url: '#',
+        cName: 'nav-Links'
+    },
+    {
+        title: 'Skills',
+        url: '#',
+        cName: 'nav-Links'
+    },
+    {
+        title: 'Experiences',
+        url: '#',
+        cName: 'nav-Links'
+    },
+    {
+        title: 'CV Download',
+        url: '#',
+        cName: 'nav-Links'
+    },
+    {
+        title: 'Sign Up',
+        url: '#',
+        cName: 'nav-Links-mobile'
+    }
+]
