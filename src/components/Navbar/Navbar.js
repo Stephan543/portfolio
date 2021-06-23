76af3daffdd9e9eb8d0512647dbf1729b92f3633
Stephan Iskander <stephan.iskander@gmail.com>
2021-06-23 18:18:58 -0400

    Playing around with material ui

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,6 +4,62 @@ import { Button } from '../Button'
 import './Navbar.css'
 import DarkModeToggle from '../../DarkModeToggle';
 
+// New Material ui dependencies
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+// import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+
+// const useStyles = makeStyles ((theme) => ({
+//     root: {
+//         flexGrow: 1
+//     },
+//     menuButton: {
+//         marginRight: theme.spacing(2)
+//     },
+//     title: {
+//         flexGrow: 1
+//     }
+// }));
+
+// export default function ButtonAppBar() {
+//     const classes = useStyles();
+
+//     return (
+//         <div className={classes.root}>
+//             <AppBar position="fixed">
+//                 <Toolbar align="left">  
+//                     <Typography variant="h6" align="left" className={classes.title}>
+
+//                     <ul className="nav-menu" align="left">
+//                         {MenuItems.map((item, index) => {
+//                             return(
+//                                 <li key={index}>
+//                                     <a className={item.cName} href={item.url}>
+//                                         {item.title}
+//                                     </a>
+//                                 </li>
+//                             )
+//                         })}
+//                     </ul>
+//                     </Typography>
+//                     <IconButton
+//                         edge="start"
+//                         classname={classes.menuButton}
+//                         color="inherit"
+//                         aria-label="menu"
+//                     >
+//                         <MenuIcon />
+//                     </IconButton>
+//                     {/* <Button color="inherit">Test</Button> */}
+//                 </Toolbar>
+//                 </AppBar>
+//         </div>
+//     )
+// }
 class Navbar extends Component {
     state = { clicked: false }
 
