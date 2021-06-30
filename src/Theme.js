79bd6f64cfc985556001796399079823cf0c33cb
Stephan Iskander <stephan.iskander@gmail.com>
2021-06-30 10:19:57 -0400

    Adding in default themes for MUI

---
+++ src/Theme.js
@@ -0,0 +1,11 @@
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme, makeStyles} from '@material-ui/core/styles';
+
+export const darkTheme = createMuiTheme({
+    palette: {
+      type: 'dark',
+    },
+  });
+
+
+export const lightTheme = createMuiTheme({});
