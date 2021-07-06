74f3f92a27299cfd029098f7aebf1689d570cb0b
Stephan Iskander <stephan.iskander@gmail.com>
2021-07-06 17:11:51 -0400

    Adding in theming at the top level

--- src/App.js
+++ src/App.js
@@ -4,16 +4,33 @@ import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/sty
 import Typography from '@material-ui/core/Typography'
 import Navbar from './components/Navbar/Navbar';
 import { darkTheme, lightTheme } from './Theme';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 
+const theme = createMuiTheme({
+  root: {
+
+  }
+})
 
 function App() {
+  const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark');
+
+  const theme = React.useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          type: prefersDarkMode ? 'dark' : 'light',
+        },
+      }),
+      [prefersDarkMode],
+  );
+
   return (
     <ThemeProvider theme={darkTheme}>
       <div className="App">
         <Navbar />
       </div> 
-    </ThemeProvider>
-    
+    </ThemeProvider> 
   );
 }
 
