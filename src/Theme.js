581c910931c719caab77895a41aed29421b418bf
Stephan Iskander <stephan.iskander@gmail.com>
2021-07-08 21:04:43 -0400

    Adding in default palette and dark mode

--- src/Theme.js
+++ src/Theme.js
@@ -1,11 +1,17 @@
 import { red } from '@material-ui/core/colors';
 import { createMuiTheme, makeStyles} from '@material-ui/core/styles';
-
-export const darkTheme = createMuiTheme({
+  
+  
+  export const theme = createMuiTheme({
     palette: {
-      type: 'dark',
+      type: 'light',
+      primary: {
+        // Purple and green play nicely together.
+        main: 'rgba(73,63,252,1)',
+      },
+      secondary: {
+        main: '#3acbf7',
+      },
     },
+    spacing: 24,
   });
-
-
-export const lightTheme = createMuiTheme({});
