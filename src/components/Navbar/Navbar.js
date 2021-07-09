1e387dec9d812beb6f31abda88f9c9d06b3d4a6b
Stephan Iskander <stephan.iskander@gmail.com>
2021-07-08 21:07:14 -0400

    fully integrated a new styled Material UI Appbar, toolbar and Tabs for navigation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,32 +1,32 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import { MenuItems } from './MenuItems';
-import { Button } from '../Button'
-import './Navbar.css'
+// import { Button } from '../Button'
+// // import './Navbar.css'
 import DarkModeToggle from '../../DarkModeToggle';
 
 import stephanCV from '../../Static/Stephan_Iskander_Resume.pdf';
 
-// React router
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    Link
-  } from "react-router-dom";
+// // React router
+// import {
+//     BrowserRouter as Router,
+//     Switch,
+//     Route,
+//     Link
+//   } from "react-router-dom";
 
 import {About} from "../Pages/About";
 import {Experience} from "../Pages/Experience";
 import {Skills} from "../Pages/Skills";
 import {Projects} from "../Pages/Projects";
 
-// New Material ui dependencies
-import { makeStyles } from "@material-ui/core/styles";
-import AppBar from "@material-ui/core/AppBar";
-import Toolbar from "@material-ui/core/Toolbar";
-import Typography from "@material-ui/core/Typography";
-// import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
-import MenuIcon from "@material-ui/icons/Menu";
+// // New Material ui dependencies
+// import { makeStyles, useMediaQuery, useTheme } from "@material-ui/core";
+// import AppBar from "@material-ui/core/AppBar";
+// import Toolbar from "@material-ui/core/Toolbar";
+// import Typography from "@material-ui/core/Typography";
+// // import Button from "@material-ui/core/Button";
+// import IconButton from "@material-ui/core/IconButton";
+// import MenuIcon from "@material-ui/icons/Menu";
 
 // const useStyles = makeStyles ((theme) => ({
 //     root: {
@@ -76,51 +76,132 @@ import MenuIcon from "@material-ui/icons/Menu";
 //     )
 // }
 
-class Navbar extends Component {
-    state = { clicked: false }
+// class Navbar extends Component {
+//     state = { clicked: false }
 
-    // Click Handle for mobile burger menu
-    handleClick = () => {
-        this.setState({ clicked: !this.state.clicked })
+//     // Click Handle for mobile burger menu
+//     handleClick = () => {
+//         this.setState({ clicked: !this.state.clicked })
+//     }
+
+//     render(){
+//         return(
+//             <Router>
+//                 <nav className="NavbarItems" color='secondary'>
+//                 {/* <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1> */}
+//                 <h1 className="navbar-button">
+//                     <DarkModeToggle />
+//                 </h1>
+//                 <div className="menu-icon" onClick={this.handleClick}>
+//                     <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+//                 </div>
+//                 <ul className={this.state.clicked ? 'nav-menu active': 'nav-menu'}>
+//                     {MenuItems.map((item, index) => {
+//                         return(
+//                             <li key={index}>
+//                                 <a className={item.cName} href={item.url}>
+//                                     {item.title}
+//                                 </a>
+//                             </li>
+//                         )
+//                     })}
+//                 </ul>
+//                 <a href={stephanCV} target="_blank" rel="noopener noreferrer">
+//                     <Button>Download CV</Button>
+//                 </a>
+//                 </nav>
+
+//                 {/* Look through switches routes and renders the first match; */}
+//                 <Switch>
+//                     <Route exact path="/" component={About} />
+//                     <Route exact path="/Skills" component={Skills} />
+//                     <Route exact path="/Experience" component={Experience} />
+//                     <Route exact path="/Projects" component={Projects} />
+//                 </Switch>
+//             </Router>
+//         )
+//     }
+// }
+
+import React, { useState } from 'react'
+import { 
+    Button, Menu, MenuItem, MenuList, useMediaQuery, useTheme
+} from '@material-ui/core'
+import { makeStyles } from '@material-ui/styles';
+import { AppBar } from '@material-ui/core';
+import { Toolbar } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
+import { IconButton } from '@material-ui/core';
+import MenuIcon from "@material-ui/icons/Menu"
+import { Tab, Tabs } from '@material-ui/core';
+
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    tabsContainer: {
+        marginLeft: 'auto',
+    },
+    menuButton: {   // Burger Respoinsive
+        marginLeft: theme.spacing(2),
+    },
+    title: {
+        flexGrow: 1,
+    },
+    downloadButton: {
+        marginLeft: 'auto',
+        color: '#fff',
+        '&:hover': {
+            background: '#fff',
+            color: '#6568F4',
+        },
+
     }
+}));
 
-    render(){
-        return(
-            <Router>
-                <nav className="NavbarItems" color='secondary'>
-                {/* <h1 className="navbar-logo">React<i className="fab fa-react"></i></h1> */}
-                <h1 className="navbar-button">
-                    <DarkModeToggle />
-                </h1>
-                <div className="menu-icon" onClick={this.handleClick}>
-                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
-                </div>
-                <ul className={this.state.clicked ? 'nav-menu active': 'nav-menu'}>
-                    {MenuItems.map((item, index) => {
-                        return(
-                            <li key={index}>
-                                <a className={item.cName} href={item.url}>
-                                    {item.title}
-                                </a>
-                            </li>
-                        )
-                    })}
-                </ul>
-                <a href={stephanCV} target="_blank" rel="noopener noreferrer">
-                    <Button>Download CV</Button>
-                </a>
-                </nav>
 
-                {/* Look through switches routes and renders the first match; */}
-                <Switch>
-                    <Route exact path="/" component={About} />
-                    <Route exact path="/Skills" component={Skills} />
-                    <Route exact path="/Experience" component={Experience} />
-                    <Route exact path="/Projects" component={Projects} />
-                </Switch>
-            </Router>
-        )
-    }
+const Navbar = () => {
+    const classes = useStyles();
+    const [anchorEl, setAnchorEL] = useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEL(event.currentTarget);
+    };
+
+    const handleClose = () =>{
+        setAnchorEL(null);
+    };
+
+
+
+    // Breakpoints
+    const theme = useTheme();
+    const [value, setValue] = useState(0);
+
+    const handleClickTab = (e, newValue) => {
+        setValue(newValue)
+    };
+
+    return (
+    
+        <div className={classes.root}>
+            <AppBar color='primary' position="fixed">
+                <Toolbar>
+                    <Typography>
+                        <DarkModeToggle/>
+                    </Typography>
+                    <Tabs className={classes.tabsContainer} onChange={handleClickTab} indicatorColor='secondary' value={value}>
+                        {MenuItems.map((item, index) =>
+                            <Tab value={index} label={item.title}/>
+                        )}
+                    </Tabs>
+                    <Button className={classes.downloadButton} color='secondary' variant='contained'>Download CV</Button>
+                </Toolbar>
+            </AppBar>
+        </div>
+        
+    );
 }
 
 export default Navbar
