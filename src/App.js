b6d97abc2b4945f812860165cf07195001686ab8
Stephan Iskander <stephan.iskander@gmail.com>
2021-07-15 11:05:04 -0400

    Adding in switch to fix the routing glitches

--- src/App.js
+++ src/App.js
@@ -1,35 +1,23 @@
 import React from 'react'
 import './App.css';
-import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
-import Typography from '@material-ui/core/Typography'
+import { ThemeProvider } from '@material-ui/core/styles';
 import Navbar from './components/Navbar/Navbar';
-import { darkTheme, lightTheme } from './Theme';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { createTheme } from '@material-ui/core/styles';
 import {theme} from './Theme.js'
-import { CssBaseline, Paper } from '@material-ui/core'
-import { BrowserRouter as Router, Route, Switch, Redirect  } from 'react-router-dom';
+import { CssBaseline } from '@material-ui/core'
+import { Route, Switch, Redirect  } from 'react-router-dom';
 
 
 const App = () => {
-  // const prefersDarkMode = useMediaQuery('(prefers-color-scheme: dark');
-  // const theme = React.useMemo(
-  //   () =>
-  //     createMuiTheme({
-  //       palette: {
-  //         type: prefersDarkMode ? 'dark' : 'light',
-  //       },
-  //     }),
-  //     [prefersDarkMode],
-  // );
 
   return (
       <ThemeProvider theme={theme}>
         <CssBaseline/>
         {/* <Navbar theme={theme} />  */}
-
-        {/* <Redirect exact from="" to="/about"></Redirect> */}
-        <Route exact path="/:page?" render={props => <Navbar {...props}/>}/>
+        <Switch>
+          <Redirect exact from="/" to="/about"></Redirect>
+          <Route exact path="/:page?" render={props => <Navbar {...props}/>}/>
+        </Switch>
+        
         
       
       </ThemeProvider> 
