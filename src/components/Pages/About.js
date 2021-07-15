aefe6ea6dd780995b06e285dac28bd7ebadb7223
Stephan Iskander <stephan.iskander@gmail.com>
2021-07-15 11:05:27 -0400

    File Cleanup

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -2,7 +2,6 @@ import React from 'react'
 import { makeStyles } from '@material-ui/core/styles';
 import Avatar from '@material-ui/core/Avatar';
 import Pic from './../../Static/Headshot.PNG';
-import Typography from '@material-ui/core/Typography';
 
 const useStyles = makeStyles((theme) => ({
     root: {
