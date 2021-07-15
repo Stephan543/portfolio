e99d70330e0785243b197e377e9364400d54cf7c
Stephan Iskander <stephan.iskander@gmail.com>
2021-07-15 11:07:26 -0400

    Fixing main route redirect path and cleaning up the file

--- src/components/Navbar/MenuItems.js
+++ src/components/Navbar/MenuItems.js
@@ -1,27 +1,18 @@
 export const MenuItems = [
     {
         title: 'About',
-        url: '/',
-        cName: 'nav-links'
+        url: '/about',
     },
     {
         title: 'Projects',
         url: '/projects',
-        cName: 'nav-links'
     },
     {
         title: 'Skills',
         url: '/skills',
-        cName: 'nav-links'
     },
     {
         title: 'Experience',
         url: '/experience',
-        cName: 'nav-links'
     },
-    // {
-    //     title: 'Download CV',
-    //     url: '#',
-    //     cName: 'nav-links-mobile'
-    // }
 ]
