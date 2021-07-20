33ede79539d4282712c39bb2a191bf8e53d6b15a
Stephan Iskander <stephan.iskander@gmail.com>
2021-07-20 09:08:46 -0400

    Adding in light and dark theme objects as well as callback function

--- src/Theme.js
+++ src/Theme.js
@@ -1,16 +1,38 @@
 import { createMuiTheme } from '@material-ui/core/styles';
   
-  
-  export const theme = createMuiTheme({
-    palette: {
-      type: 'dark',
-      primary: {
-        // Purple and green play nicely together.
-        main: 'rgba(73,63,252,1)',
-      },
-      secondary: {
-        main: '#3acbf7',
-      },
+export const dark = createMuiTheme({
+  palette: {
+    type: 'dark',
+    primary: {
+      // Purple and green play nicely together.
+      main: 'rgba(73,63,252,1)',
     },
-    // spacing: 24,
-  });
+    secondary: {
+      main: '#3acbf7',
+    },
+  },
+  // spacing: 24,
+});
+
+export const light = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      // Purple and green play nicely together.
+      main: 'rgba(73,63,252,1)',
+    },
+    secondary: {
+      main: '#3acbf7',
+    },
+  },
+  // spacing: 24,
+});
+
+const themes = {
+  light,
+  dark,
+}
+
+export default function getTheme(theme){
+  return themes[theme]
+}
