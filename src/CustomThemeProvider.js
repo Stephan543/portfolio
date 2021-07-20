3cfbcbbc7e20f43b5cffeda9c24058e9c9ff5711
Stephan Iskander <stephan.iskander@gmail.com>
2021-07-20 09:09:23 -0400

    Added in a global context provider that will keep track of theme as a state

---
+++ src/CustomThemeProvider.js
@@ -0,0 +1,43 @@
+import React, { useState }  from 'react';
+import { ThemeProvider } from '@material-ui/styles';
+import getTheme from './Theme.js';
+
+export const CustomThemeContext = React.createContext(
+    {
+        currentTheme: 'light',
+        setTheme: null,
+    },
+)
+
+const CustomThemeProvider = (props) => {
+
+    const { children } = props;
+
+    // Read Current theme from local storage or set to default dark
+    const currentTheme = localStorage.getItem('appTheme') || 'dark';
+
+    // State to hold the theme type
+    const [themeName, _setThemeName] = useState(currentTheme);
+
+    // Retrieving the theme name
+    const theme = getTheme(themeName);
+
+    // Setting the local storage
+    const setThemeName = (name) => {
+        localStorage.setItem('appTheme', name);
+        _setThemeName(name);
+    };
+
+    const contextValue = {
+        currentTheme: themeName,
+        setTheme: setThemeName,
+    }
+
+    return (
+        <CustomThemeContext.Provider value={contextValue}>
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </CustomThemeContext.Provider>
+    );
+}
+
+export default CustomThemeProvider;
