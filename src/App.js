0db0b022db6e1851cc5b839279079bd25c8e56b9
Stephan Iskander <stephan.iskander@gmail.com>
2021-07-20 09:09:43 -0400

    Removed old theming provider

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import './App.css';
-import { ThemeProvider } from '@material-ui/core/styles';
 import Navbar from './components/Navbar/Navbar';
-import {theme} from './Theme.js'
 import { CssBaseline } from '@material-ui/core'
 import { Route, Switch, Redirect  } from 'react-router-dom';
+import CustomThemeProvider from './CustomThemeProvider';
+
+
 
 const App = () => {
 
   return (
-      <ThemeProvider theme={theme}>
-        <CssBaseline/>
-        <Switch>
-          <Redirect exact from="/" to="/about"></Redirect>
-          <Route exact path="/:page?" render={props => <Navbar {...props}/>}/>
-        </Switch>      
-      </ThemeProvider> 
+    <CustomThemeProvider>
+      <CssBaseline/>
+      <Switch>
+        <Redirect exact from="/" to="/about"></Redirect>
+        <Route exact path="/:page?" render={props => <Navbar {...props}/>}/>
+      </Switch>      
+    </CustomThemeProvider> 
   );
 }
 
