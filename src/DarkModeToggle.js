7bb7b1e5b2ed32a3ccc3744649743ed21205c7b6
Stephan Iskander <stephan.iskander@gmail.com>
2021-07-23 16:01:58 -0400

    Code clean up

--- src/DarkModeToggle.js
+++ src/DarkModeToggle.js
@@ -1,15 +1,12 @@
 import React from 'react'
 import Switch from '@material-ui/core/Switch';
-import {FaMoon, FaSun} from 'react-icons/fa';
 import Brightness7Icon from '@material-ui/icons/Brightness7';
 import Brightness4Icon from '@material-ui/icons/Brightness4';
 import { CustomThemeContext } from './CustomThemeProvider';
 import { useContext } from 'react';
-import { IconButton } from '@material-ui/core';
 
 const DarkModeToggle = () => {
-    // const [theme, setTheme] = useState(true);
-    // const appliedTheme = createMuiTheme(theme ? light : dark);
+
     const {currentTheme, setTheme} = useContext(CustomThemeContext)
     const isDark = Boolean(currentTheme === 'dark')
     
@@ -37,63 +34,5 @@ const DarkModeToggle = () => {
     )
 }
 
-
-// class DarkModeToggle extends Component {
-//     constructor(props){
-//         super(props);
-
-//         if(JSON.parse(localStorage.getItem('DARK_MODE')) === true){ // Checks local storage for clients preference
-//             document.body.classList.add('dark-mode');
-//         }
-
-//         this.state = { // sets state to dark mode
-//             darkMode: JSON.parse(localStorage.getItem('DARK_MODE'))
-//         }
-
-//         this.handleModeChange = this.handleModeChange.bind(this); // Must bing in order for 'this' to work in callback
-//     }
-
-//     handleModeChange() {
-//         if(!this.state.darkMode) { // Check for state congruency on dark mode
-//             // document.body.classList.add('dark-mode');
-//             theme.palette.type = 'dark';
-//         } else {
-//             // document.body.classList.remove('dark-mode');
-//             theme.palette.type = 'light';
-//         }
-
-//         this.setState({
-//             darkMode: (!this.state.darkMode)
-//         });
-
-//         localStorage.setItem('DARK_MODE', !this.state.darkMode); // Save on clients storage
-//     }
-    
-//     render(){
-        
-//         return (
-           
-//             /* <button className="btn btn--small" onClick={this.handleModeChange}>Change Mode</button> */
-//             <Switch 
-//             onChange={this.handleModeChange}
-//             icon={<FaMoon />}
-//             checkedIcon={<FaSun />}
-//             />
-           
-//         );
-//     }
-// }
-
 export default DarkModeToggle;
 
-// export const light = {
-//     palette: {
-//       type: "light"
-//     }
-//   };
-//   export const dark = {
-//     palette: {
-//       type: "dark"
-//     }
-//   };
-  
