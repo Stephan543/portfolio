5a499be6cc89642deffc2c35722563109d4eee56
Stephan Iskander <stephan.iskander@gmail.com>
2021-07-26 12:01:35 -0400

    File Clean up

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react'
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
-import { CssBaseline } from '@material-ui/core'
 import { Route, Switch, Redirect  } from 'react-router-dom';
-import CustomThemeProvider from './CustomThemeProvider';
 
 const App = () => {
 
